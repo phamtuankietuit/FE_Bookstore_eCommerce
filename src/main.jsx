71222571dc548d38e9c2efe0a9de00568ab819f6
Phạm Tuấn Kiệt <103875393+phamtuankietuit@users.noreply.github.com>
2024-09-15 20:18:16 +0700

    update path 3

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { ToastProvider } from "./contexts/ToastContext.jsx"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/FE_Bookstore_eCommerce'>
       <AuthProvider>
         <ToastProvider>
           <Routes>
